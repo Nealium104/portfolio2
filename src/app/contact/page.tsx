144511779f6922a790ee794000b94de86df7547d
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-07-10 15:17:04 -0400

    I forgot to escape my '

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -7,7 +7,7 @@ export default function Contact() {
     <Nav />
     <main className="text-4xl">
     <h1 className='font-playfair'>Hi there</h1>
-      <h2 className='font-source'>We're currently under construction 👷‍♂️</h2>
+      <h2 className='font-source'>We&apos;re currently under construction 👷‍♂️</h2>
       <span>Take a look at the other pages for now and feel free to reach out using any of the links at the bottom!</span>
     </main>
   </>
--- src/app/music/page.tsx
+++ src/app/music/page.tsx
@@ -7,7 +7,7 @@ export default function Music() {
     <Nav />
     <main className="text-4xl">
     <h1 className='font-montserrat'>Hi there</h1>
-      <h2 className='font-source'>We're currently under construction 👷‍♂️</h2>
+      <h2 className='font-source'>We&apos;re currently under construction 👷‍♂️</h2>
       <span>Take a look at the other pages for now and feel free to reach out using any of the links at the bottom!</span>
     </main>
   </>
